42a9e2494d29c565964e6863777457f456155b8e
Yoan Mollard <yoan.mollard@inria.fr>
2015-10-06 16:18:53 +0200

    Added a simple Face Commander

--- src/baxter_commander/__init__.py
+++ src/baxter_commander/__init__.py
@@ -1,2 +1,3 @@
 from . commander import ArmCommander
+from . face import FaceCommander
 from . persistence import *
---
+++ src/baxter_commander/face.py
@@ -0,0 +1,36 @@
+import rospy
+import transformations
+from numpy import arccos, isnan, pi
+from baxter_interface import Head
+from tf import TransformListener
+
+__all__ = ['FaceCommander']
+
+class FaceCommander(Head):
+    def __init__(self):
+        Head.__init__(self)
+        self._world = 'base'
+        self._tf_listener = TransformListener()
+
+    def look_at(self, obj):
+        if isinstance(obj, str):
+            pose = self._tf_listener.lookupTransform('head', obj, rospy.Time(0))
+        else:
+            rospy.logerr("FaceCommander.look_at() accepts only strings atm")
+            return False
+
+        hyp = transformations.norm(pose)
+        adj = pose[0][1]
+        angle = pi/2 - arccos(float(adj)/hyp)
+
+        if isnan(angle):
+            rospy.logerr('FaceCommander cannot look at {}'.format(obj))
+            return False
+
+        self.set_pan(angle)
+        return True
+
+
+
+
+
