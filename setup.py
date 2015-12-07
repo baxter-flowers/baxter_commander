2fb37df713316461c1ccf3fc85b2902661c0f131
Yoan Mollard <yoan.mollard@inria.fr>
2015-12-07 16:26:29 +0100

    xmltodict is a python dependency

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ d = generate_distutils_setup()
 d['packages'] = ['baxter_commander', 'transformations']
 d['package_dir'] = {'': 'src'}
 d['requires'] = ['baxter_pykdl', 'baxter_interface', 'baxter_core_msgs', 'actionlib_msgs', 'moveit_msgs', 'trajectory_msgs', 'control_msgs', 'geometry_msgs']
+d['install_requires'] = ['xmltodict']
 setup(**d)
