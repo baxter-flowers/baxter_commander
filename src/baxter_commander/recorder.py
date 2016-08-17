071e0ba05a8f3b135506ac65d48c1c2fb6c494e7
Yoan Mollard <yoan.mollard@inria.fr>
2016-08-17 15:16:41 +0200

    Bugfixes rare bugs recorder

--- src/baxter_commander/recorder.py
+++ src/baxter_commander/recorder.py
@@ -1,5 +1,5 @@
 import rospy
-from threading import Lock
+from threading import RLock
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
 from geometry_msgs.msg import PoseStamped
@@ -10,28 +10,30 @@ from baxter_core_msgs.msg import EndEffectorState
 __all__ = ['Recorder']
 
 class Recorder:
-    def __init__(self, side='both', max_points=1000000):
+    def __init__(self, side, max_points=1000000):
         """
         Constructs a recorder in task space and joint space
-        :param side: both, left or right
+        :param side: left or right
         :param max_points: Maximum number of trajectory points to be recorded
         """
         self._side = side
+        self._joints = map(lambda j: self._side + '_' + j, ['s0', 's1', 'e0', 'e1', 'w0', 'w1', 'w2'])
         self.max_n_points = max_points
+        self._recording_lock = RLock()
         self._recording = False
         self._recorder_rate = -1
 
         self._joint_last_recorded_point = rospy.Time(0)
-        self._joint_recorder_lock = Lock()
+        self._joint_recorder_lock = RLock()
         self._joint_recorded_points = []
         self._joint_recorded_times = []
 
         self._eef_last_recorded_point = rospy.Time(0)
-        self._eef_recorder_lock = Lock()
+        self._eef_recorder_lock = RLock()
         self._eef_recorded_points = []
         self._eef_recorded_times = []
 
-        sides = ['left', 'right', 'both']
+        sides = ['left', 'right']
         if side not in sides:
             raise ValueError("Side '{}' can't be recorded. Accepted values are {}".format(side, str(sides)))
 
@@ -39,31 +41,34 @@ class Recorder:
         rospy.Subscriber("/robot/limb/{}/endpoint_state".format(side), EndEffectorState, self._cb_eef_state)
 
     def _cb_joint_state(self, msg):
-        if len(self._joint_recorded_points) < self.max_n_points:
-            now = rospy.Time.now()
-            if self._recording and self._joint_last_recorded_point + rospy.Duration(1./self._recorder_rate) < now:
-                self._joint_last_recorded_point = now
-                with self._joint_recorder_lock:
-                    self._joint_recorded_points.append(msg)
-                    self._joint_recorded_times.append(msg.header.stamp)
+        with self._joint_recorder_lock:
+            if len(self._joint_recorded_points) < self.max_n_points:
+                now = rospy.Time.now()
+                with self._recording_lock:
+                    if self._recording and self._joint_last_recorded_point + rospy.Duration(1./self._recorder_rate) < now:
+                        self._joint_last_recorded_point = now
+                        self._joint_recorded_points.append(msg)
+                        self._joint_recorded_times.append(msg.header.stamp)
 
     def _cb_eef_state(self, msg):
-        if len(self._eef_recorded_points) < self.max_n_points:
-            now = rospy.Time.now()
-            if self._recording and self._eef_last_recorded_point + rospy.Duration(1./self._recorder_rate) < now:
-                self._eef_last_recorded_point = now
-                with self._eef_recorder_lock:
-                    self._eef_recorded_points.append(msg)
-                    self._eef_recorded_times.append(msg.header.stamp)
+        with self._eef_recorder_lock:
+            if len(self._eef_recorded_points) < self.max_n_points:
+                now = rospy.Time.now()
+                with self._recording_lock:
+                    if self._recording and self._eef_last_recorded_point + rospy.Duration(1./self._recorder_rate) < now:
+                        self._eef_last_recorded_point = now
+                        self._eef_recorded_points.append(msg)
+                        self._eef_recorded_times.append(msg.header.stamp)
 
     def start(self, rate_hz=50.):
         """
         Start the recording of end effector and joint points at the specified rate
         :param rate_hz: Recording rate
         """
-        if self._recording:
-            rospy.logerr("Recorder is already recording {} arm".format(str(self._side)))
-            return
+        with self._recording_lock:
+            if self._recording:
+                rospy.logerr("Recorder is already recording {} arm".format(str(self._side)))
+                return
         self._recorder_rate = rate_hz
         with self._joint_recorder_lock:
             self._joint_recorded_points = []
@@ -71,7 +76,8 @@ class Recorder:
         with self._eef_recorder_lock:
             self._eef_recorded_points = []
             self._eef_recorded_times = []
-        self._recording = True
+        with self._recording_lock:
+            self._recording = True
 
     def stop(self, include_velocity=False, include_effort=False):
         """
@@ -80,18 +86,19 @@ class Recorder:
         :param include_effort:  if True, the joint efforts will be included in the returned trajectory
         :return: (joint_traj, eef_traj) resp. the recorded joint trajectory (RobotTrajectory) and end effector trajectory in frame 'base' (Path)
         """
-        self._recording = False
+        with self._recording_lock:
+            self._recording = False
 
         # Now we reconstruct a RobotTrajectory...
         rt = RobotTrajectory()
-        rt.joint_trajectory.joint_names = [joint for joint in self._joint_recorded_points[0].name if self._side in joint or
-                                           self._side == 'both'] if len(self._joint_recorded_points) > 0 else []
+        rt.joint_trajectory.joint_names = self._joints
+
         with self._joint_recorder_lock:
             for idx, js in enumerate(self._joint_recorded_points):
                 jtp = JointTrajectoryPoint()
                 try:
                     jtp.positions = [js.position[js.name.index(joint)] for joint in rt.joint_trajectory.joint_names]
-                except ValueError:
+                except ValueError as e:
                     # This specific JS sample does not contain the desired joints, skip it
                     continue
                 else:
